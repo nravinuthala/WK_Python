ecd57632f11cba4428080018523777fc32b8b605
Nagaraj Ravinuthala <nagarajr@nexaria.com>
2022-06-24 16:57:31 +0530

    code refactor

--- assignments/vowel_count.py
+++ assignments/vowel_count.py
@@ -3,14 +3,22 @@ inputstr = inputstr.casefold()
 print(inputstr)
 vowels = 'aeiou'
 
+# vlist = []
+# for char in inputstr:
+#     if char in vowels:
+#         vlist.append(char)
+#
+# print(vlist)
+#
 # count total number of vowels
 num_vowels = [vowel for vowel in inputstr if vowel in vowels]
 print((len(num_vowels), num_vowels))
 
 # count number of times each vowel occurs
 vowel_count = {}.fromkeys(vowels, 0)
+print(vowel_count)
 for char in inputstr:
     if char in vowels:
         vowel_count[char] += 1
 
-print(vowel_count)
+# print(vowel_count)
